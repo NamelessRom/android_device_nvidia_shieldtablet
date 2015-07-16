2bc1fd2efe991af4690b70ade9c55cb21deacb03
Alexander Martinz <eviscerationls@gmail.com>
2015-07-16 10:35:09 +0200

    initial ramdisk and TWRP setup
      * the shield tablet is weird, we need to supply shared libraries
        with the recovery ramdisk to get the touchscreen working
    
    Change-Id: Iac69ffd643e10897b792a3c2006dbc9c438f39a0
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,72 @@
+#
+# Copyright (C) 2015 The NamelessRom Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_SPECIFIC_HEADER_PATH := device/nvidia/shieldtablet/include
+
+# Architecture
+TARGET_ARCH         := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI      := armeabi-v7a
+TARGET_CPU_ABI2     := armeabi
+TARGET_CPU_VARIANT  := cortex-a15
+
+# Board
+TARGET_BOARD_PLATFORM := tegra
+TARGET_NO_BOOTLOADER  := true
+TARGET_NO_RADIOIMAGE  := true
+
+# Kernel
+TARGET_KERNEL_ARCH   := arm
+TARGET_KERNEL_SOURCE := kernel/nvidia/shieldtablet
+TARGET_KERNEL_CONFIG := custom_shieldtablet_defconfig
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
+
+BOARD_CUSTOM_BOOTIMG_MK := device/nvidia/shieldtablet/mkbootimg.mk
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_USERDATAIMAGE_PARTITION_SIZE  := 12799754240
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1342177280
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := shieldtablet,wx_un_do
+
+# Recovery
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+COMMON_GLOBAL_CPPFLAGS += -DNO_SECURE_DISCARD
+TARGET_RECOVERY_DEVICE_DIRS += device/nvidia/shieldtablet
+TARGET_RECOVERY_FSTAB := device/nvidia/shieldtablet/rootdir/etc/fstab.tn8
+
+# TWRP
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+BOARD_HAS_FLIPPED_SCREEN := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_SCREEN_BLANK := true
+TW_EXCLUDE_ENCRYPTED_BACKUPS := true
+TW_INCLUDE_L_CRYPTO := true
+
+TW_NO_USB_STORAGE := true
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+
+TW_BRIGHTNESS_PATH := /sys/class/backlight/pwm-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
