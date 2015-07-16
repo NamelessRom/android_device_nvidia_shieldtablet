ccd2c15ffb9691b25011be9eaf1a6f757f9e5e46
Alexander Martinz <eviscerationls@gmail.com>
2015-07-16 16:00:23 +0200

    mediacodecs: welcome to the new age
    Change-Id: Id8c9887aef140442c4b5d9b63fbe97ef488a5ad1
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- nameless_shieldtablet.mk
+++ nameless_shieldtablet.mk
@@ -96,7 +96,10 @@ PRODUCT_PACKAGES += \
 # Media config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
